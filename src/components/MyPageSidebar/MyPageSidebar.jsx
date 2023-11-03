9fa871cc4675877b6c22d1b4dd8b1823d4037a51
JIWOO\Administrator <jw7812@naver.com>
2023-11-03 11:42:15 +0900

    Feat: mypage-layout-구현-#15

--- src/components/MyPageSidebar/MyPageSidebar.jsx
+++ src/components/MyPageSidebar/MyPageSidebar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { css } from '@emotion/react';
+/** @jsxImportSource @emotion/react */
+import * as S from "./Style"
 
 function MyPageSidebar(props) {
     return (
-        <div>
+        <div css={S.SMySidebarBox}>
+            <div>내정보</div>
+            <div>닉네임</div>
+            <div>관심 학원 0개</div>
+            <div>개인 정보 수정</div>
+            <div>나의 학원 문의</div>
+            <div>작성한 후기</div>
             
         </div>
     );
