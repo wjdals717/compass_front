6f437d9c65f098c9ce980bf2985b547d32cdeedd
mini3100 <mini3100@naver.com>
2023-11-06 11:21:00 +0900

    Feat: 회원가입 기능 구현 완료 - 로그인 기능 구현 중

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,28 +2,32 @@ import React, { useState } from 'react';
 import { css } from '@emotion/react';
 /** @jsxImportSource @emotion/react */
 import * as S from "./Style"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useQueryClient } from 'react-query';
 
 function Header(props) {
 
+    const queryClient = useQueryClient();
+    const principalState = queryClient.getQueryState("getPrincipal");
+
+    const navigate = useNavigate();
+
     const [ isOpen, setIsOpen ] = useState(false);
-    const [ isLogin, setIsLogin ] = useState(false);
 
     const handleDropdownBtnOnClick = () => {
-        setIsOpen(!isOpen)
+        setIsOpen(!isOpen);
     }
 
     const handleLoginOnClick = () => {
-
-        setIsLogin(true)
+        navigate("/auth/signin");
     }
 
     const handleLogoutOnClick = () => {
-        alert("로그아웃 하시겠습니까?")
-        setIsLogin(false)
+        if(window.confirm("로그아웃 하시겠습니까?")) {
+            localStorage.removeItem("accessToken");
+            window.location.replace("/");
+        }
     }
-    
-    
 
     return (
         <div css={S.SLayout}>
@@ -35,18 +39,9 @@ function Header(props) {
                 </div>
             </div>
             <div css={S.SLoginButtonBox}>
-                {isLogin ? (
-                    <div></div>
-                ) : (
-                    <div>
-                        <Link to={"/auth/signin"} onClick={handleLoginOnClick}>로그인</Link>
-                    </div>
-                )}
-            </div>
-
-            {isLogin && (
-                <div css={S.SDropDownbar}>
-                    <button css={S.SDropBtn} onClick={handleDropdownBtnOnClick}>ooo 님</button>
+                {!!principalState?.data?.data ? (
+                    <div css={S.SDropDownbar}>
+                    <button css={S.SDropBtn} onClick={handleDropdownBtnOnClick}>{principalState.data.data.nickname} 님</button>
                     <div css={[S.SDropDownContent, isOpen && S.SDropDownContentVisible]}>
                         <div css={S.SDropDownbarMenu}>
                             <div css={S.SBtn1}><Link to="/account/mypage">마이페이지</Link></div>
@@ -54,7 +49,12 @@ function Header(props) {
                         </div>
                     </div>
                 </div>
-            )}
+                ) : (
+                    <div>
+                        <Link to={"/auth/signin"} onClick={handleLoginOnClick}>로그인</Link>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
