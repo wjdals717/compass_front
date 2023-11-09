93898dff9ad9fd4d5794989b3cacf0e19983b408
JIWOO\Administrator <jw7812@naver.com>
2023-11-09 17:37:39 +0900

    Feat: role_id 값에 따라 MyPageSidebar 변환 #46

--- src/components/MyPageSidebar/MyPageSidebar.jsx
+++ src/components/MyPageSidebar/MyPageSidebar.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { css } from '@emotion/react';
 /** @jsxImportSource @emotion/react */
 import * as S from "./Style";
 import {AiFillSetting} from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import { useQueryClient } from 'react-query';
+import WebMastesrSidebar from './WebMastesrSidebar/WebMastesrSidebar';
+import StudentSidebar from './StudentSidebar/StudentSidebar';
+import AcademySidebar from './AcademySidebar/AcademySidebar';
 
 function MyPageSidebar({ role, children }) {
+
+    const queryClient = useQueryClient();
+    const principalState = queryClient.getQueryState("getPrincipal")
+    const principal = principalState.data.data;
+    const [ roleMypageSidebar, setRoleMypageSidebar ] = useState(null);
+    console.log(principal)
+
+    useEffect(() => {
+        if (principal.roleId !== roleMypageSidebar) {
+            setRoleMypageSidebar(principal.roleId);
+        }
+    }, [principal.roleId, roleMypageSidebar]);
+
     return (
         <div css={S.MySidebarLayout}>
             <div css={S.UserContainer}>
@@ -14,8 +31,12 @@ function MyPageSidebar({ role, children }) {
                     🍀<span>{role}</span>
                 </div>
             </div>
+            <div>
+                
+            </div>
             { children }
         </div>
+        
     );
 }
 
