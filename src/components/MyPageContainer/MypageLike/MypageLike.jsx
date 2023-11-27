0f024fe64258f31b10c05f244bed4d608756a467
mini3100 <mini3100@naver.com>
2023-11-27 12:12:32 +0900

    Feat: 관심 학원 페이지네이션

--- src/components/MyPageContainer/MypageLike/MypageLike.jsx
+++ src/components/MyPageContainer/MypageLike/MypageLike.jsx
@@ -1,39 +1,33 @@
-
 import React, { useState } from 'react';
 import { css } from '@emotion/react';
 /** @jsxImportSource @emotion/react */
 import * as S from "./Style";
 import { useQuery, useQueryClient } from 'react-query';
 import { instance } from '../../../api/config/instance';
-import { useNavigate } from 'react-router-dom';
 import LiAcademyBox from '../../LiAcademyBox/LiAcademyBox';
-import LinkBtn from '../../Button/LinkBtn/LinkBtn';
 import EmptyBox from '../../EmptyBox/EmptyBox';
+import Pagination from '../../Pagination/Pagination';
+import { useParams } from 'react-router';
 
 function MypageLike(props) {
-    // 랜덤 색상을 생성하는 함수
-    const getRandomColor = () => {
-    // 0부터 255 사이의 랜덤한 RGB 값 생성
-    const randomColor = `rgb(${Math.floor(Math.random() * 127 + 128)}, ${Math.floor(Math.random() * 127 + 128)}, ${Math.floor(Math.random() * 127 + 128)})`;
-    return randomColor;
-    };
 
-    const navigate = useNavigate();
+    const { page } = useParams();
 
     const queryClient = useQueryClient();
     const principalState = queryClient.getQueryState("getPrincipal")
     const principal = principalState?.data?.data;
+    const likeCountOfMypage = queryClient.getQueryState("getLikeCountOfMypage");
 
     const userId = principal?.userId
 
-    const getLikeAcademiesQuery = useQuery(["getLikeAcademies"], async () => {
+    const getLikeAcademiesQuery = useQuery(["getLikeAcademies", page], async () => {
         try {
             const option = {
                 headers: {
                     Authorization: localStorage.getItem("accessToken")
                 }
             }
-            return await instance.get(`/account/mypage/like/${userId}`, option);
+            return await instance.get(`/account/mypage/like/${userId}/${page}`, option);
         } catch (error) {
             console.error(error);
         }
@@ -42,7 +36,7 @@ function MypageLike(props) {
         refetchOnWindowFocus: false
     });
 
-    if(getLikeAcademiesQuery.isLoading) {
+    if(likeCountOfMypage.isLoading || getLikeAcademiesQuery.isLoading) {
         return <></>;
     }
     
@@ -50,13 +44,14 @@ function MypageLike(props) {
         <div>
             <h2>❤️ 나의 관심 학원</h2>
             <div>
-                {getLikeAcademiesQuery.data.data.length === 0 ? 
+                {likeCountOfMypage === 0 ? 
                 <EmptyBox comment={"나의 관심 학원을 추가해 보세요!"} link={'/academy/find/1'} btn={"보러 가기"}/> : 
                 <ul css={S.UlBox}>
                     {!getLikeAcademiesQuery.isLoading && Array.isArray(getLikeAcademiesQuery?.data?.data) && getLikeAcademiesQuery?.data?.data.map(academy => {
                         return <LiAcademyBox key={academy.ACADEMY_ID} academy={academy}/>
                     })}
                 </ul>}
+                <Pagination totalCount={likeCountOfMypage.data.data} link={'/account/mypage/like'} search={''}/>
             </div>
         </div>
     );
